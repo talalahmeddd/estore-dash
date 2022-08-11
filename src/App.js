3c3163f218747ea71bcc0feb413f360c164eecaf
Talalahmeddd <talal.ahmed109@gmail.com>
2022-08-11 09:28:05 +0500

    changes

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Dashboard from "./components/dashboard/dashboard";
+import Navbar from "./components/navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="AppGlass">
+        <Navbar />
+        <Dashboard />
+      </div>{" "}
+    </div>
+  );
+}
+
+export default App;
+//hello world
---
+++ src/components/Table/Table.jsx
@@ -0,0 +1,84 @@
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import "./Table.css";
+
+function createData(name, trackingId, date, status) {
+  return { name, trackingId, date, status };
+}
+
+const rows = [
+  createData("Iphone 11 pro", 10001, "10 August 2022", "Approved"),
+  createData("Iphone 12 pro", 10002, "10 August 2022", "Pending"),
+  createData("Iphone 13 pro", 10003, "10 August 2022", "Approved"),
+  createData("Iphone 13 pro max", 10004, "10 August 2022", "Delivered"),
+];
+
+
+const makeStyle=(status)=>{
+  if(status === 'Approved')
+  {
+    return {
+      background: 'rgb(145 254 159 / 47%)',
+      color: 'green',
+    }
+  }
+  else if(status === 'Pending')
+  {
+    return{
+      background: '#ffadad8f',
+      color: 'red',
+    }
+  }
+  else{
+    return{
+      background: '#59bfff',
+      color: 'white',
+    }
+  }
+}
+
+export default function BasicTable() {
+  return (
+      <div className="Table">
+      <h3>Recent Orders</h3>
+        <TableContainer
+          style={{ boxShadow: "2px 13px 20px 2px #80808029", border: "0px solid #000000"}}
+        >
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Product</TableCell>
+                <TableCell align="left">Tracking ID</TableCell>
+                <TableCell align="left">Date</TableCell>
+                <TableCell align="left">Status</TableCell>
+                <TableCell align="left"></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody style={{ color: "white" }}>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="left">{row.trackingId}</TableCell>
+                  <TableCell align="left">{row.date}</TableCell>
+                  <TableCell align="left">
+                    <span className="status" style={makeStyle(row.status)}>{row.status}</span>
+                  </TableCell>
+                  <TableCell align="left" className="Details">Details</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+  );
+}
